7cdaa98f0c4b23f7fac33c809918d06b9cf56345
Tyler Posadovsky <typosado@genband.com>
2017-02-04 20:02:45 -0500

    added logog

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 //import background from '../../../public/images/login_background.jpg';
-
+import logo from "../../../public/images/logo.png"
 
 class Main extends Component {
 	render(){
 		return (
 			<div className="app-login">
-				asd
+				<div className="logo"><img src={logo}/></div>
+				<div className="login-button"></div>
 			</div>
 		)
 	}
