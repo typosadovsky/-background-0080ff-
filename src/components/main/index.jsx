aef453da7df9571b7e3a036a2702eb60eb630299
Tyler Posadovsky <typosado@genband.com>
2017-02-04 20:26:52 -0500

    added fb login

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 
 //import background from '../../../public/images/login_background.jpg';
 import logo from "../../../public/images/logo.png"
+import facebook_logo from "../../../public/images/facebook.png"
 
 class Main extends Component {
 	render(){
 		return (
 			<div className="app-login">
 				<div className="logo"><img src={logo}/></div>
-				<div className="login-button"></div>
+				<div className="login-button"><img className="facebook_image" src={facebook_logo}/></div>
 			</div>
 		)
 	}
